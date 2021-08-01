04ee40d8dcf26e1be78c8c693a20902d3b7b1dff
ElenaKuikka <gagaelena@yandex.ru>
2021-08-01 12:36:39 +0500

    Initial commit

---
+++ HomeWork_6/HomeWork_6/String.cpp
@@ -0,0 +1,221 @@
+#include "String.h"
+
+String::String(const char *newStr) {
+	size = strlen(newStr) + 1;
+	array = allocatorString.allocate(size);
+	for (int i = 0; i < size - 1; i++) {
+		array[i] = newStr[i];
+	}
+	array[size - 1] = '\0';
+}
+
+String::String(const String &object) : size{ object.size }, array{ allocatorString.allocate(object.size) }{
+	for (int i = 0; i < size; i++) {
+		array[i] = object.array[i];
+	}
+}
+
+void String::setNewArray() {
+	std::cout << "¬ведите строку и нажмите Enter.\n";
+	if (size < 80) {
+		size = 80;
+		allocatorString.deallocate(array);
+		array = allocatorString.allocate(size);
+	}
+	for (int i = 0; i < size - 1; i++) {
+		array[i] = _getch();
+		std::cout << array[i];
+		if (array[i] == 13) {
+			break;
+		}
+	}
+	std::cout << std::endl;
+}
+
+void String::setArray(char *arr) {
+	int TempSize(strlen(arr) + 1);
+	if (array) {
+		allocatorString.deallocate(array);
+	}
+	array = allocatorString.allocate(TempSize);
+	for (int i = 0; i < TempSize - 1; i++) {
+		array[i] = arr[i];
+	}
+	array[TempSize - 1] = '/0';
+}
+
+void String::PrintArray() {
+	std::cout << "¬аша строка: \n";
+	int i{ 0 };
+	while (array[i] != '\0') {
+		std::cout << array[i];
+		i++;
+	}
+	std::cout << "\n";
+}
+
+char &String::operator[](const int index) {
+	if (index < 0 || index >= size) {
+		throw std::exception("Ёлемента с заданным индексом не существует!");
+	}
+	else {
+		return array[index];
+	}
+}
+
+const char &String::operator[](const int index) const {
+	if (index < 0 || index >= size) {
+		throw std::exception("Ёлемента с заданным индексом не существует!");
+	}
+	else {
+		return array[index];
+	}
+}
+
+int String::operator()(const char elem) {
+	for (int i = 0; i < size; i++) {
+		if (array[i] == elem) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+String &String::operator++() {
+	char *temp = allocatorString.allocate(size);
+	for (int i = 0; i < size; i++) {
+		temp[i] = array[i];
+	}
+	allocatorString.deallocate(array);
+	array = allocatorString.allocate(size + 1);
+	for (int i = 0; i < size - 1; i++) {
+		array[i] = temp[i];
+	}
+	array[size - 1] = ' ';
+	array[size] = '\0';
+	++size;
+	allocatorString.deallocate(temp);
+	return *this;
+}
+
+String &String::operator--() {
+	if (size>=2)
+	{
+		char *temp = allocatorString.allocate(size);
+		for (int i = 0; i < size; i++) {
+			temp[i] = array[i];
+		}
+		allocatorString.deallocate(array);
+		array = allocatorString.allocate(--size);
+		for (int j = 0; j < size - 1; j++) {
+			array[j] = temp[j];
+		}
+		array[size - 1] = '\0';
+		allocatorString.deallocate(temp);
+		return *this;
+	}
+	else{
+		return *this;
+	}
+}
+
+String String::operator++(int) {
+	String temp(*this);
+	++(*this);
+	return temp;
+}
+
+String String::operator--(int) {
+	if (size>=2)
+	{
+		String temp(*this);
+		--(*this);
+		return temp;
+	}
+	else{
+		return *this;
+	}
+}
+
+String String::operator+(String &one) {
+	int resultSize = size + one.size - 1;
+	String result(resultSize);
+	int i{ 0 };
+	for (int j = 0; j < size - 1; j++) {
+		result[i] = array[j];
+		i++;
+	}
+	for (int k = 0; k < one.size; k++) {
+		result[i] = one.array[k];
+		i++;
+	}
+	return result;
+}
+
+std::ostream &operator<< (std::ostream &out, const String &one) {
+	int i{ 0 };
+	while (one[i] != '\0') {
+		out << one[i];
+		i++;
+	}
+	return out;
+}
+
+std::istream& operator>> (std::istream &in, String &one) {
+	in >> one.array;
+	one.addMemory(one.array);
+	one.trimm();
+	return in;
+}
+
+void String::addMemory(char *newStr) {
+	int newSize{ 0 };
+	newSize = strlen(newStr) + 1;
+	if (newSize <= size) {
+		for (int i = 0; i < newSize - 1; i++) {
+			array[i] = newStr[i];
+		}
+		array[newSize - 1] = '\0';
+	}
+	else {
+		array = nullptr;
+		array = allocatorString.allocate(newSize);
+		for (int j = 0; j < newSize - 1; j++) {
+			array[j] = newStr[j];
+		}
+		array[newSize - 1] = '\0';
+		size = newSize;
+	}
+}
+
+void String::free() {
+	if (array) {
+		array = nullptr;
+		size = 0;
+	}
+}
+
+void String::trimm() {
+	if (array) {
+		int i{ 0 };
+		while (array[i] != '\0') {
+			i++;
+		}
+		i++;
+		if (i < size) {
+			char *temp = allocatorString.allocate(i);
+			for (int j = 0; j < i - 1; j++) {
+				temp[j] = array[j];
+			}
+			temp[i - 1] = '\0';
+			allocatorString.deallocate(array);
+			array = allocatorString.allocate(i);
+			for (int k = 0; k < i; k++) {
+				array[k] = temp[k];
+			}
+			size = i;
+			allocatorString.deallocate(temp);
+		}
+	}
+}
+
---
+++ HomeWork_6/HomeWork_6/String.h
@@ -0,0 +1,88 @@
+#ifndef STRING
+
+#define STRING
+
+#include<iostream>
+#include<conio.h>
+#include<bitset>
+#include<math.h>
+
+class String
+{
+public:
+	String(int sizeS) : size{ sizeS }, array{ allocatorString.allocate(sizeS) } {}
+	String() :String(80) {}
+	String(const char *newStr);
+	String(const String &object);
+
+	virtual void setNewArray();
+
+	int getSize() {
+		return size;
+	}
+
+	char *getArray() {
+		return array;
+	}
+
+	void setSize(int nSize){
+		size = nSize;
+	}
+
+	void setArray(char *arr);
+
+	void PrintArray();
+
+	char &operator[](const int index);
+
+	const char &operator[](const int index) const;
+
+	int operator()(const char elem);
+
+	//Возвращает длину строки
+	virtual operator int() { return size; }
+
+	String &operator++();
+
+	String operator++(int);
+
+	String &operator--();
+
+	String operator--(int);
+
+	String operator+(String &one);
+
+	friend std::ostream &operator<< (std::ostream &out, const String &one);
+
+	friend std::istream &operator>> (std::istream &in, String &one);
+	
+	void addMemory(char *newStr);
+
+	void free();
+
+	void trimm();
+	
+	~String() {
+		allocatorString.deallocate(array);
+	}
+	
+protected:
+	struct AllocatorString {
+		char *allocate(int size) const {
+			return new char[size] {0};
+		}
+
+		void deallocate(char *arr) {
+			if (arr)delete[] arr;
+		}
+
+	}allocatorString;
+private:
+	
+	int size;
+	char *array;
+};
+
+
+
+#endif STRING
